1cdeb51f927cfc9d1cb9d29ca045f816c1ae387b
ashok <ashokmarripudi@gmail.com>
2018-12-03 23:34:10 +0530

    Angular Cli

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aaksolProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the aaksolProjectApp
+ */
+angular.module('aaksolProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
